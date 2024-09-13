60fffcca7419778a3577ad96be25b43ece2e9f0f
arhamfareed106 <arhamfareed575@gmail.com>
2024-09-14 00:55:38 +0500

    indend

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ con.addEventListener("dblclick", function() {
     love.style.transform = "translate(-50%, -50%) scale(1)"; 
     love.style.opacity = 0.8;
 
+    
     setTimeout(function() {
         love.style.opacity = 0;
     }, 1000);
